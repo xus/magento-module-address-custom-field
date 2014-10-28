7f8ce898fdc67fe56c3d5a1251a9ad04157c560e
xus <jesuslopezreyes@gmail.com>
2014-10-28 17:07:25 +0100

    First commit

---
+++ app/code/local/Xus/Barrio/sql/customerattribute1414496206_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,41 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+
+$installer->addAttribute("customer_address", "barrio",  array(
+    "type"     => "varchar",
+    "backend"  => "",
+    "label"    => "barrio",
+    "input"    => "text",
+    "source"   => "",
+    "visible"  => true,
+    "required" => true,
+    "default" => "",
+    "frontend" => "",
+    "unique"     => false,
+    "note"       => ""
+
+	));
+
+        $attribute   = Mage::getSingleton("eav/config")->getAttribute("customer_address", "barrio");
+
+        
+$used_in_forms=array();
+
+$used_in_forms[]="adminhtml_customer_address";
+$used_in_forms[]="customer_register_address";
+$used_in_forms[]="customer_address_edit";
+        $attribute->setData("used_in_forms", $used_in_forms)
+		->setData("is_used_for_customer_segment", true)
+		->setData("is_system", 0)
+		->setData("is_user_defined", 1)
+		->setData("is_visible", 1)
+		->setData("sort_order", 100)
+		;
+        $attribute->save();
+	
+	
+	
+$installer->endSetup();
+	 
